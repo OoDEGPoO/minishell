ab68df48ccd1badee214580d09b7f3daa7a86911
OoDEGPoO <Oo.DEGP.oO@gmail.com>
2015-05-03 21:12:56 +0200

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -12,7 +12,6 @@ int main (int argc, char *argv[])
    char buf[BUFSIZ];
    char *cadena; /*string para almacenar las ordenes introducidas*/
    bool continuar = true; /*Booleano para cuando se introduzca exit terminar la ejecución*/
-   int salida;
 
    while (continuar)
    {
@@ -21,24 +20,22 @@ int main (int argc, char *argv[])
 	cadena = strtok(buf, ";"); /*almacena las ordenes separadas en tokens*/
 	//printf("%s\n", cadena); /*para probar que se ha almacenado de forma adecuada "cadena"*/
 
-	//while (cadena != NULL){ //cuando se acaban las ordenes termina
+	while (cadena != NULL){ //cuando se acaban las ordenes termina
 		if ((strcmp(cadena,"exit"))!=0){ //compara la primera orden con "exit"
 			if ((ord_interna(cadena))==0){
 				/*si la orden es interna da 1 y la ejecuta, si es externa entra al if*/
 				//printf("es externa\n"); /*para comprobar que la orden es externa*/
 				ejecutar_linea_ordenes(cadena);
 			}
-			cadena = strtok(NULL, ";"); //pasamos al siguiente tokens y portanto la siguiente orden
-			salida = 0;
 		}
 		else {
 			continuar = false;
-			salida = 0;
 		}
-	//}
+		cadena = strtok(NULL, ";"); //pasamos al siguiente tokens y portanto la siguiente orden
+	}
    }
 
-   return salida;
+   return 0;
 }
 
 
