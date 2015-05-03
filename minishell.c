3318e477188ec792924151ba12d54bf815dd323e
OoDEGPoO <Oo.DEGP.oO@gmail.com>
2015-05-03 22:40:10 +0200

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -33,9 +33,8 @@ int main (int argc, char *argv[])
 		cadena = strtok(NULL, ";"); //pasamos al siguiente tokens y portanto la siguiente orden
 	}
 	fflush(stdin);
+	cadena = NULL;
    }
 
    return 0;
 }
-
-
